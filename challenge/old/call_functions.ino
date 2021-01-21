c8573f4a87e48658f0634c649da67fd0d72d22a9
Alex_ <69150061+KTsuyuri@users.noreply.github.com>
2021-01-21 11:54:50 +0100

    Add files via upload

---
+++ challenge/old/call_functions.ino
@@ -0,0 +1,79 @@
+#define ENA 5
+#define ENB 6
+#define IN1 7
+#define IN2 8
+#define IN3 9
+#define IN4 11
+
+void forward(){ 
+  digitalWrite(ENA,HIGH); 
+  digitalWrite(ENB,HIGH); 
+  digitalWrite(IN1,HIGH); 
+  digitalWrite(IN2,LOW);  
+  digitalWrite(IN3,LOW);  
+  digitalWrite(IN4,HIGH); 
+}
+
+void back(){
+  digitalWrite(ENA,HIGH);
+  digitalWrite(ENB,HIGH);
+  digitalWrite(IN1,LOW);
+  digitalWrite(IN2,HIGH);
+  digitalWrite(IN3,HIGH);
+  digitalWrite(IN4,LOW);
+}
+
+void right(){
+  digitalWrite(ENA,HIGH);
+  digitalWrite(ENB,HIGH);
+  digitalWrite(IN1,HIGH);
+  digitalWrite(IN2,LOW);
+  digitalWrite(IN3,HIGH);
+  digitalWrite(IN4,LOW);
+}
+
+void left(){
+  digitalWrite(ENA,HIGH);
+  digitalWrite(ENB,HIGH);
+  digitalWrite(IN1,LOW);
+  digitalWrite(IN2,HIGH);
+  digitalWrite(IN3,LOW);
+  digitalWrite(IN4,HIGH); 
+}
+
+void retour() {
+    back();
+    delay(1000);
+    right();
+    delay(300);
+    back();
+    delay(1000);
+    left();
+    delay(300);
+    back();
+    delay(1000);
+}
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(IN1,OUTPUT); 
+  pinMode(IN2,OUTPUT);
+  pinMode(IN3,OUTPUT);
+  pinMode(IN4,OUTPUT);
+  pinMode(ENA,OUTPUT);
+  pinMode(ENB,OUTPUT);
+}
+
+void loop() {
+    forward();
+    delay(1000);
+    right();
+    delay(300);
+    forward();
+    delay(1000);
+    left();
+    delay(300);
+    forward();
+    delay(1000);
+    retour();
+}
